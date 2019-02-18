0844c25d282a35b518778c42b97f0750fbff2b7e
Ferdinand Malcher <mail@ferdinand-malcher.de>
2019-02-18 15:02:49 +0100

    RxJS Playground

---
+++ rxjs-playground/src/app/exercises/1_fromevent/fromevent.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { map, startWith, debounceTime } from 'rxjs/operators';
+
+@Component({
+  selector: 'rxw-fromevent',
+  templateUrl: './fromevent.component.html',
+  styles: []
+})
+export class FromeventComponent implements OnInit {
+
+  currentWidth = 0;
+
+  ngOnInit() {
+
+    /******************************/
+
+
+
+    /******************************/
+  }
+
+}
