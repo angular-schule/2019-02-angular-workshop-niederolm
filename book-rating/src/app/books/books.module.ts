fc19719669f995926c5c940d11385c9bb54e0d27
Ferdinand Malcher <mail@ferdinand-malcher.de>
2019-02-18 16:45:57 +0100

    BookDetailsComponent

--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -5,10 +5,12 @@ import { BooksRoutingModule } from './books-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ButtonSharedModule } from '../button-shared/button-shared.module';
 import { BooksSharedModule } from 'books-shared';
+import { BookDetailsComponent } from './book-details/book-details.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
+    BookDetailsComponent,
   ],
   imports: [
     CommonModule,
